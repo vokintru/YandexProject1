b9929ad141fd1eb38df138d630fe2f9746b5f34c
vokintru <aodplayer@bk.ru>
2023-10-15 12:24:19 +0300

    Добавлено открытие файла

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import sys
 import io
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QFileDialog, QWidget, QTableWidgetItem
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.file = ""
         uic.loadUi('qt.ui', self)  # Загружаем дизайн
         self.btnOpen.clicked.connect(self.open)
         self.btnSave.clicked.connect(self.save)
 
-
     def open(self):
-        print('Clicked "Open"')
-
+        self.file = QFileDialog.getOpenFileName(self, 'Выбрать файл', '')[0]
+        print(self.file)
 
     def save(self):
         print('Clicked "Save"')
