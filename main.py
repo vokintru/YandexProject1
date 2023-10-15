cae64c119dc7d084d9e69a81e8d1e7b41e9d37b5
vokintru <aodplayer@bk.ru>
2023-10-15 12:16:20 +0300

    Add:  - Design  - Some code

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import io
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('qt.ui', self) # Загружаем дизайн
+
+
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.excepthook = except_hook
+    ex.show()
+    sys.exit(app.exec())
