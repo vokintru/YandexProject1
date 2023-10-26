ff3e27638c880ca84dc39a98ea0497d22433c923
vokintru <aodplayer@bk.ru>
2023-10-26 17:25:26 +0300

    Изменение проекта

--- main.py
+++ main.py
@@ -1,55 +1,31 @@
+import sqlite3
+
+from PyQt5.QtWidgets import QWidget, QApplication, QListWidgetItem, QMessageBox
+from PyQt5.uic import loadUi
 import sys
-import io
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QFileDialog, QWidget, QTableWidgetItem
 
 
-class MainWindow(QWidget):
+class Window(QWidget):
     def __init__(self):
-        super().__init__()
-        uic.loadUi('qt.ui', self)  # Загружаем дизайн
-        self.btnOpen.clicked.connect(self.open)
-        self.btnSave.clicked.connect(self.save)
-        self.file = "temp"
-        self.f = open(self.file, "w")
+        super(Window, self).__init__()
+        loadUi("qt.ui", self)
+        self.calendarWidget.selectionChanged.connect(self.calendarDateChanged)
+        self.calendarDateChanged()
+        self.saveButton.clicked.connect(self.saveChanges)
+        self.addButton.clicked.connect(self.addNewTask)
 
-    def open(self):
-        try:
-            self.file = QFileDialog.getOpenFileName(self, 'Выбрать файл', '',
-                                                    'Текстовый файл (*.txt);;')[0]
-            self.f = open(self.file, "r+")
-            self.textEdit.setPlainText(self.f.read())
-        except UnicodeDecodeError:
-            self.status.setText("Ошибка декодирования (UnicodeDecodeError)")
-        except Exception:
-            self.status.setText("Неизвестная ошибка")
+    def calendarDateChanged(self):
+        pass
 
-    def save(self):
-        if self.file != "temp":
-            self.f.close()
-            self.f = open(self.file, "w")
-            self.f.write(self.textEdit.toPlainText())
-            self.f.close()
-            self.f = open(self.file, "r+")
-            self.textEdit.setPlainText(self.f.read())
-        else:
-            self.file = QFileDialog.getOpenFileName(self, 'Выбрать файл', '',
-                                                    'Текстовый файл (*.txt);;')[0]
-            self.f.close()
-            self.f = open(self.file, "w")
-            self.f.write(self.textEdit.toPlainText())
-            self.f.close()
-            self.f = open(self.file, "r+")
-            self.textEdit.setPlainText(self.f.read())
+    def saveChanges(self):
+        pass
+
+    def addNewTask(self):
+        pass
 
 
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = MainWindow()
-    sys.excepthook = except_hook
-    ex.show()
+    window = Window()
+    window.show()
     sys.exit(app.exec())
